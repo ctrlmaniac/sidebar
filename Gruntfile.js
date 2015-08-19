776341cf45bede9a4af4d8518bb8ceb3830855c6
dcdeiv <davide.dicriscito@gmail.com>
2015-08-19 14:55:25 +0200

    fix identation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,63 +1,63 @@
 module.exports = function(grunt) {
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-        banner: '/*! <%= pkg.name %> v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
-                '** <%= pkg.author %>\n' +
-                '** (c) 2015-<%= grunt.template.today("yyyy") %> Licensed under <%= pkg.license %>\n' +
-                '*/',
+    banner: '/*! <%= pkg.name %> v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
+      '** <%= pkg.author %>\n' +
+      '** (c) 2015-<%= grunt.template.today("yyyy") %> Licensed under <%= pkg.license %>\n' +
+      '*/',
 
-        jshint: {
-            options: {
-                curly: true,
-                eqeqeq: true,
-                eqnull: true,
-                browser: true,
-                globals: {
-                    jQuery: true
-                },
-            },
-            gruntfile: {
-                src: 'Gruntfile.js'
-            },
-            src: {
-                src: ['src/**/*.js']
-            }
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        eqnull: true,
+        browser: true,
+        globals: {
+          jQuery: true
         },
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      src: {
+        src: ['src/**/*.js']
+      }
+    },
 
-        concat: {
-            options: {
-                banner: '<%= banner %>\n',
-                stripBanners: true
-            },
-            dist: {
-                src: ['src/simpler-sidebar.js'],
-                dest: 'dist/jquery.<%= pkg.name %>.js'
-            }
-        },
+    concat: {
+      options: {
+        banner: '<%= banner %>\n',
+        stripBanners: true
+      },
+      dist: {
+        src: ['src/simpler-sidebar.js'],
+        dest: 'dist/jquery.<%= pkg.name %>.js'
+      }
+    },
 
-        uglify: {
-            options: {
-                banner: '<%= banner %>\n'
-            },
-            dist: {
-                src: '<%= concat.dist.dest %>',
-                dest: 'dist/jquery.<%= pkg.name %>.min.js'
-            }
-        },
+    uglify: {
+      options: {
+        banner: '<%= banner %>\n'
+      },
+      dist: {
+        src: '<%= concat.dist.dest %>',
+        dest: 'dist/jquery.<%= pkg.name %>.min.js'
+      }
+    },
 
-        watch: {
-            files: ['src/simpler-sidebar.js'],
-            tasks: ['jshint', 'concat', 'uglify']
-        }
-    });
+    watch: {
+      files: ['src/simpler-sidebar.js'],
+      tasks: ['jshint', 'concat', 'uglify']
+    }
+  });
 
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
 
-    // Default task.
-    grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
 };
