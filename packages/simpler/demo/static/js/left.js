62028d4ca0b3430156c2b6a7e46d52871d3b5cf6
Davide Di Criscito <davide.dicriscito@gmail.com>
2020-07-09 15:42:55 +0200

    chore(simpler-sidebar): fix demo

--- packages/simpler/demo/static/js/left.js
+++ packages/simpler/demo/static/js/left.js
@@ -1,3 +1,12 @@
 $("document").ready(function () {
-  $("#sidebar-left").simplerSidebar();
+  $("#sidebar-left").simplerSidebar({
+    align: "left",
+    selectors: {
+      trigger: "#toggle-sidebar-left",
+      quitter: ".quit-sidebar-left",
+    },
+    animation: {
+      easing: "easeOutQuint",
+    },
+  });
 });
