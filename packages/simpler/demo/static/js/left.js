26800a4ec458bede6061fe62e95d7325b0969f62
Davide Di Criscito <davide.dicriscito@gmail.com>
2020-07-09 23:02:30 +0200

    docs: update demo

--- packages/simpler/demo/static/js/left.js
+++ packages/simpler/demo/static/js/left.js
@@ -2,7 +2,7 @@ $("document").ready(function () {
   $("#sidebar-left").simplerSidebar({
     selectors: {
       trigger: "#toggle-sidebar-left",
-      quitter: ".quit-sidebar-left",
+      quitter: ".quit-sidebar",
     },
     animation: {
       easing: "easeOutQuint",
