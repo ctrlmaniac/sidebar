0533e01a5cde5f021f35c0c2b842c07c0bdd71c2
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-09-07 23:03:12 +0200

    feat(modern): strip comments

--- packages/modern/lib/modern.js
+++ packages/modern/lib/modern.js
@@ -11,29 +11,20 @@ class ModernSidebar {
         this.easing =
             typeof opt.easing === "undefined" ? "ease-in-out" : opt.easing;
         this.zIndex = typeof opt.zIndex === "undefined" ? 3000 : opt.zIndex;
-        // other variables
         const position = this.align == "right" ? "left" : "right";
-        // Select the elements
         this.sidebar = document.querySelector(this.selector);
         this.triggerer = document.querySelector(opt.triggerer);
-        // Add attributes
         this.sidebar.setAttribute("data-status", this.opened ? "open" : "closed");
-        // Add z-index
         this.sidebar.style.zIndex = this.zIndex.toString();
-        // Add transition
         this.sidebar.style.transition = `${this.align} 500ms ${this.easing}`;
-        // Set sidebar position
         this.sidebar.style.position = "fixed";
         this.sidebar.style.top = this.top;
         this.sidebar.style.bottom = "0";
-        // Set sidebar width
         this.sidebar.style.width = "100%";
         this.sidebar.style.maxWidth = this.width;
-        // Let the mask be displayed when the screen is narrower than the sidebar
         if (window.innerWidth <= parseInt(this.width) + this.gap) {
             this.sidebar.style.width = `${(window.innerWidth - this.gap).toString()}px`;
         }
-        // set sidebar width to let the mask to be displayed when screen get narrower
         window.onresize = () => {
             const safeWidth = `${(window.innerWidth - this.gap).toString()}px`;
             if (window.innerWidth <= parseInt(this.width) + this.gap) {
@@ -42,19 +33,16 @@ class ModernSidebar {
             else {
                 this.sidebar.style.width = "100%";
             }
-            // Fix sidebar position
             if (this.sidebar.getAttribute("data-status") == "closed") {
                 this.sidebar.style[position] = `-${safeWidth}`;
             }
         };
-        // Set sidebar open or closed
         if (this.opened) {
             this.sidebar.style[this.align] = "0px";
         }
         else {
             this.sidebar.style[this.align] = `-${this.sidebar.offsetWidth}px`;
         }
-        // Trigger sidebar animation
         this.triggerer.onclick = () => {
             const status = this.sidebar.getAttribute("data-status");
             if (status === "closed") {
@@ -64,7 +52,6 @@ class ModernSidebar {
                 this.close();
             }
         };
-        // Create Mask
         this.mask = document.createElement("div");
         this.mask.setAttribute("data-mask", this.opened ? "open" : "closed");
         this.mask.style.display = "none";
