4951ddb672dceb7ac64419a83847f23220d1fd6f
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-09-08 00:05:39 +0200

    refactor: init simpler-css3

---
+++ packages/simpler-css3/README.md
@@ -0,0 +1,11 @@
+# `simpler-css3`
+
+> TODO: description
+
+## Usage
+
+```
+const simplerCss3 = require('simpler-css3');
+
+// TODO: DEMONSTRATE API
+```
--- packages/simpler/__tests__/simpler.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const simpler = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(simpler(), 'Hello from simpler');
-console.info("simpler tests passed");
