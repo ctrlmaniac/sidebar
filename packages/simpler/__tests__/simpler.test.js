235efb790d71309bf5bdca6feb29e38c98000bc0
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-09-08 00:03:00 +0200

    refactor: init simpler package

---
+++ packages/simpler/__tests__/simpler.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const simpler = require('..');
+const assert = require('assert').strict;
+
+assert.strictEqual(simpler(), 'Hello from simpler');
+console.info("simpler tests passed");
---
+++ packages/simpler/lib/simpler.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = simpler;
+
+function simpler() {
+    return "Hello from simpler";
+}
