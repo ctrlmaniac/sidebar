64a0add3717ab04cee5f52522dfb1d9d9ead8936
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-09-08 08:09:54 +0200

    refactor: build simpler-sidebar-css3

---
+++ packages/simpler-css3/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    jquery: true,
+    browser: true,
+    es2021: true,
+  },
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+  },
+  rules: {},
+};
