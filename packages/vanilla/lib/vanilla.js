e58e966b0b2cc718395d495db1652b9223bb2027
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-09-08 08:15:02 +0200

    refactor: add vanilla

--- packages/vanilla/lib/vanilla.js
+++ packages/vanilla/lib/vanilla.js
@@ -1,7 +1,96 @@
-'use strict';
-
-module.exports = vanilla;
-
-function vanilla() {
-    return "Hello from vanilla";
+"use strict";
+class ModernSidebar {
+    constructor(opt) {
+        this.selector =
+            typeof opt.selector === "undefined" ? "#sidebar" : opt.selector;
+        this.align = typeof opt.align === "undefined" ? "left" : opt.align;
+        this.top = typeof opt.top === "undefined" ? "56px" : opt.top;
+        this.width = typeof opt.width === "undefined" ? "300px" : opt.width;
+        this.gap = typeof opt.gap === "undefined" ? 56 : parseInt(opt.gap);
+        this.opened = typeof opt.opened === "undefined" ? false : opt.opened;
+        this.easing =
+            typeof opt.easing === "undefined" ? "ease-in-out" : opt.easing;
+        this.zIndex = typeof opt.zIndex === "undefined" ? 3000 : opt.zIndex;
+        const position = this.align == "right" ? "left" : "right";
+        this.sidebar = document.querySelector(this.selector);
+        this.triggerer = document.querySelector(opt.triggerer);
+        this.sidebar.setAttribute("data-status", this.opened ? "open" : "closed");
+        this.sidebar.style.zIndex = this.zIndex.toString();
+        this.sidebar.style.transition = `${this.align} 500ms ${this.easing}`;
+        this.sidebar.style.position = "fixed";
+        this.sidebar.style.top = this.top;
+        this.sidebar.style.bottom = "0";
+        this.sidebar.style.width = "100%";
+        this.sidebar.style.maxWidth = this.width;
+        if (window.innerWidth <= parseInt(this.width) + this.gap) {
+            this.sidebar.style.width = `${(window.innerWidth - this.gap).toString()}px`;
+        }
+        window.onresize = () => {
+            const safeWidth = `${(window.innerWidth - this.gap).toString()}px`;
+            if (window.innerWidth <= parseInt(this.width) + this.gap) {
+                this.sidebar.style.width = safeWidth;
+            }
+            else {
+                this.sidebar.style.width = "100%";
+            }
+            if (this.sidebar.getAttribute("data-status") == "closed") {
+                this.sidebar.style[position] = `-${safeWidth}`;
+            }
+        };
+        if (this.opened) {
+            this.sidebar.style[this.align] = "0px";
+        }
+        else {
+            this.sidebar.style[this.align] = `-${this.sidebar.offsetWidth}px`;
+        }
+        this.triggerer.onclick = () => {
+            const status = this.sidebar.getAttribute("data-status");
+            if (status === "closed") {
+                this.open();
+            }
+            else {
+                this.close();
+            }
+        };
+        this.mask = document.createElement("div");
+        this.mask.setAttribute("data-mask", this.opened ? "open" : "closed");
+        this.mask.style.display = "none";
+        this.mask.style.background = "rgba(0, 0, 0, 0.8)";
+        this.mask.style.zIndex = (this.zIndex - 1).toString();
+        this.mask.style.position = "absolute";
+        this.mask.style.top = this.top;
+        this.mask.style.right = "0";
+        this.mask.style.bottom = "0";
+        this.mask.style.left = "0";
+        this.mask.style.transition = `display 500ms ${this.easing}`;
+        document.body.appendChild(this.mask);
+        this.mask.onclick = () => this.close();
+    }
+    setAttribute() {
+        const status = this.sidebar.getAttribute("data-status");
+        if (status === "closed") {
+            this.sidebar.setAttribute("data-status", "opened");
+        }
+        else {
+            this.sidebar.setAttribute("data-status", "closed");
+        }
+    }
+    open() {
+        this.sidebar.style[this.align] = "0px";
+        const status = this.sidebar.getAttribute("data-status");
+        this.setAttribute();
+        this.showMask();
+    }
+    close() {
+        this.sidebar.style[this.align] = `-${this.sidebar.offsetWidth}px`;
+        const status = this.sidebar.getAttribute("data-status");
+        this.setAttribute();
+        this.hideMask();
+    }
+    showMask() {
+        this.mask.style.display = "block";
+    }
+    hideMask() {
+        this.mask.style.display = "none";
+    }
 }
---
+++ packages/vanilla/src/vanilla.ts
@@ -0,0 +1,152 @@
+interface options {
+  selector: string;
+  triggerer: string;
+  align: "right" | "left";
+  top: string;
+  width: string;
+  gap: string;
+  opened: boolean;
+  easing: string;
+  zIndex: number;
+}
+
+class ModernSidebar {
+  sidebar: HTMLElement;
+  triggerer: HTMLElement;
+  mask: HTMLElement;
+  selector: string;
+  align: any; // must be fixed
+  top: string;
+  width: string;
+  gap: number;
+  opened: boolean;
+  easing: string;
+  zIndex: number;
+
+  constructor(opt: options) {
+    this.selector =
+      typeof opt.selector === "undefined" ? "#sidebar" : opt.selector;
+    this.align = typeof opt.align === "undefined" ? "left" : opt.align;
+    this.top = typeof opt.top === "undefined" ? "56px" : opt.top;
+    this.width = typeof opt.width === "undefined" ? "300px" : opt.width;
+    this.gap = typeof opt.gap === "undefined" ? 56 : parseInt(opt.gap);
+    this.opened = typeof opt.opened === "undefined" ? false : opt.opened;
+    this.easing =
+      typeof opt.easing === "undefined" ? "ease-in-out" : opt.easing;
+    this.zIndex = typeof opt.zIndex === "undefined" ? 3000 : opt.zIndex;
+
+    // other variables
+    const position = this.align == "right" ? "left" : "right";
+
+    // Select the elements
+    this.sidebar = document.querySelector(this.selector)!;
+    this.triggerer = document.querySelector(opt.triggerer)!;
+
+    // Add attributes
+    this.sidebar.setAttribute("data-status", this.opened ? "open" : "closed");
+
+    // Add z-index
+    this.sidebar.style.zIndex = this.zIndex.toString();
+
+    // Add transition
+    this.sidebar.style.transition = `${this.align} 500ms ${this.easing}`;
+
+    // Set sidebar position
+    this.sidebar.style.position = "fixed";
+    this.sidebar.style.top = this.top;
+    this.sidebar.style.bottom = "0";
+
+    // Set sidebar width
+    this.sidebar.style.width = "100%";
+    this.sidebar.style.maxWidth = this.width;
+
+    // Let the mask be displayed when the screen is narrower than the sidebar
+    if (window.innerWidth <= parseInt(this.width) + this.gap) {
+      this.sidebar.style.width = `${(
+        window.innerWidth - this.gap
+      ).toString()}px`;
+    }
+
+    // set sidebar width to let the mask to be displayed when screen get narrower
+    window.onresize = () => {
+      const safeWidth = `${(window.innerWidth - this.gap).toString()}px`;
+
+      if (window.innerWidth <= parseInt(this.width) + this.gap) {
+        this.sidebar.style.width = safeWidth;
+      } else {
+        this.sidebar.style.width = "100%";
+      }
+
+      // Fix sidebar position
+      if (this.sidebar.getAttribute("data-status") == "closed") {
+        this.sidebar.style[position] = `-${safeWidth}`;
+      }
+    };
+
+    // Set sidebar open or closed
+    if (this.opened) {
+      this.sidebar.style[this.align] = "0px";
+    } else {
+      this.sidebar.style[this.align] = `-${this.sidebar.offsetWidth}px`;
+    }
+
+    // Trigger sidebar animation
+    this.triggerer.onclick = () => {
+      const status: string = this.sidebar.getAttribute("data-status")!;
+
+      if (status === "closed") {
+        this.open();
+      } else {
+        this.close();
+      }
+    };
+
+    // Create Mask
+    this.mask = document.createElement("div");
+    this.mask.setAttribute("data-mask", this.opened ? "open" : "closed");
+    this.mask.style.display = "none";
+    this.mask.style.background = "rgba(0, 0, 0, 0.8)";
+    this.mask.style.zIndex = (this.zIndex - 1).toString();
+    this.mask.style.position = "absolute";
+    this.mask.style.top = this.top;
+    this.mask.style.right = "0";
+    this.mask.style.bottom = "0";
+    this.mask.style.left = "0";
+    this.mask.style.transition = `display 500ms ${this.easing}`;
+    document.body.appendChild(this.mask);
+
+    this.mask.onclick = () => this.close();
+  }
+
+  setAttribute() {
+    const status: string = this.sidebar.getAttribute("data-status")!;
+
+    if (status === "closed") {
+      this.sidebar.setAttribute("data-status", "opened");
+    } else {
+      this.sidebar.setAttribute("data-status", "closed");
+    }
+  }
+
+  open() {
+    this.sidebar.style[this.align] = "0px";
+    const status: string = this.sidebar.getAttribute("data-status")!;
+    this.setAttribute();
+    this.showMask();
+  }
+
+  close() {
+    this.sidebar.style[this.align] = `-${this.sidebar.offsetWidth}px`;
+    const status: string = this.sidebar.getAttribute("data-status")!;
+    this.setAttribute();
+    this.hideMask();
+  }
+
+  showMask() {
+    this.mask.style.display = "block";
+  }
+
+  hideMask() {
+    this.mask.style.display = "none";
+  }
+}
