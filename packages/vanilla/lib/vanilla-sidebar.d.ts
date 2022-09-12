a47e2d333e8e50da334bd68d0184b98e6b5c14e2
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-09-12 20:33:04 +0200

    feat: include declaration module

---
+++ packages/vanilla/lib/vanilla-sidebar.d.ts
@@ -0,0 +1,36 @@
+interface options {
+    selector: string;
+    triggerer: string;
+    quitter: string;
+    mask: boolean;
+    align: "right" | "left";
+    top: string;
+    width: string;
+    gap: string;
+    opened: boolean;
+    easing: string;
+    zIndex: number;
+}
+declare class VanillaSidebar {
+    sidebar: HTMLElement;
+    triggerer: HTMLElement;
+    quitter: NodeListOf<Element>;
+    mask: HTMLElement;
+    hasMask: boolean;
+    selector: string;
+    quitterSelector: string;
+    align: any;
+    top: string;
+    width: string;
+    gap: number;
+    opened: boolean;
+    easing: string;
+    zIndex: number;
+    animationDuration: string;
+    constructor(opt: options);
+    setAttribute(): void;
+    open(): void;
+    close(): void;
+    showMask(): void;
+    hideMask(): void;
+}
